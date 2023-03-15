1f3a4901473fc9ad3c9d97ec008f9e5b9ab3c0ca
Вадим Лосенков <vprog0729@gmail.com>
2023-03-15 20:13:40 +0300

    desctop: header, top, corruption, about, benefits, start projects

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 import { isWebp } from './utills.js';
 isWebp();
 
+import './form.js';
+
+import Masonry from 'masonry-layout';
+
 const documentHeight = () => {
   const doc = document.documentElement;
   doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
 };
 documentHeight();
+
+const $grid = new Masonry('.projects__box', {
+  itemSelector: '.projects__item',
+  gutter: 50,
+});
