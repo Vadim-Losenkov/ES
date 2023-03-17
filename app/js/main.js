3ffdf894fbedfcfc97d2fd095f21249f298f9e1e
Вадим Лосенков <vprog0729@gmail.com>
2023-03-17 21:26:45 +0300

    finish info, start fileBanner

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 import { isWebp } from './utills.js';
+import Swiper, { Autoplay, EffectFade, Pagination } from 'swiper';
 isWebp();
 
 import './form.js';
@@ -9,3 +10,21 @@ const documentHeight = () => {
   doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
 };
 documentHeight();
+
+const infoSlider = new Swiper('.info__slider', {
+  modules: [Pagination, EffectFade, Autoplay],
+  effect: 'fade',
+  loop: true,
+  fadeEffect: {
+    crossFade: true,
+  },
+  autoplay: {
+    delay: 5000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.info__slider-pagination',
+    type: 'progressbar',
+  },
+});
